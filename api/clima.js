41375dbd0762cc2977942a114a648cd18774a968
frank272208 <camposfranklin927@gmail.com>
2021-08-31 23:40:00 -0400

    DSFSDF

--- api/clima.js
+++ api/clima.js
@@ -3,11 +3,11 @@
 // id cochabamba 3919966
 // url api.openweathermap.org/data/2.5/weather?id=4404348&units=metric&appid={986592cac35e7c6455536dcc37352645986592cac35e7c6455536dcc37352645}
 
-const requestURL='https://api.openweathermap.org/data/2.5/weather?id=4404348&units=metric&appid={986592cac35e7c6455536dcc37352645986592cac35e7c6455536dcc37352645}'
+const requestURL='https://api.openweathermap.org/data/2.5/weather?id=4404348&units=metric&APPID=986592cac35e7c6455536dcc37352645986592cac35e7c6455536dcc37352645'
 const request = new XMLHttpRequest();
 request.open('GET',requestURL);
 request.responseType = 'json';
-//request.send();
+request.send();
 
 // obteniendo los datos json
 
